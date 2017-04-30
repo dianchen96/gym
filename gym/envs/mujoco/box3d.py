aacd0ffae2ae4ba8572e92f3510d2b52c6bc2082
Dian Chen <dianchen96@gmail.com>
2017-04-29 22:30:43 -0700

    fixed reward

--- gym/envs/mujoco/box3d.py
+++ gym/envs/mujoco/box3d.py
@@ -190,7 +190,7 @@ class Box3dFixedReachEnv(mujoco_env.MujocoEnv, utils.EzPickle):
         d = self.unwrapped.data
         # print (d.site_xpos.flatten())
         distance = np.linalg.norm(d.site_xpos.flatten() - (list(d.qpos[-2:].flatten()) + [0.025]))
-        if distance <= 0.1:
+        if distance <= 0.2:
             reach_reward += 2.0 - distance*3
         reward = reach_reward
 
